b65d1941cf0b9c89a4a77fb8e5bbf310589e3f20
okpopova <oksana.popova@sbermegamarket.ru>
2024-08-23 00:07:01 +0300

    format YabbaDabbaDo

--- lecture_1/YabbaDabbaDo.kt
+++ lecture_1/YabbaDabbaDo.kt
@@ -1,16 +1,16 @@
 package lessons.lecture_1
 
-fun main(){
-    val a=3
-    val b=1
-    println(if (a>b) "a is greater than b" else "a is not greater than b")
+fun main() {
+    val a = 3
+    val b = 1
+    println(if (a > b) "a is greater than b" else "a is not greater than b")
     println("This line runs no matter what")
 
     var x = 1
-    while (x<3){
-        print(if (x ==1) "Yab" else "Dab")
+    while (x < 3) {
+        print(if (x == 1) "Yab" else "Dab")
         print("ba")
-        x=x+1
+        x = x + 1
     }
-    if (x==3) println("Do")
+    if (x == 3) println("Do")
 }
